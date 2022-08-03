b9cd557f5cf94563111b51d374c4bdd2bb70a55d
mblanchet1-admin <blanchet.michael1@gmail.com>
2022-08-02 23:49:22 -0400

    fetch page added

--- src/fetch.js
+++ src/fetch.js
@@ -1,8 +1,5 @@
 window.onload = function () {
   document.getElementById("button").addEventListener("click", fetchPromise);
-  const table = document.createElement("p");
-  table.setAttribute("id","p1")
-  document.getElementById("tableSpace").appendChild(table);
 }
 
 async function fetchPromise() {
@@ -11,12 +8,29 @@ async function fetchPromise() {
       method: 'GET'
     });
       if(response.ok) {
-        const jsonResponse = await response.json();
-        document.getElementById("p1").innerHTML = JSON.stringify(jsonResponse);
-      }
-     } catch(error) {
+        const tableData = await response.json();
+        buildTable("skills", tableData);
+        }
+        document.getElementById("p1").innerHTML = JSON.stringify(tableData);
+      } catch(error) {
       console.log("!");
       }
     }
+
+function buildTable(tableId, data) {
+  var table = document.getElementById(tableId);
+
+  // in the following... "Object.keys(data[Object.keys(data)[0]][row])[<column>]" grabs the keys from JSON
+  // to access values. I was trying to keep it more generalized but could have just hardcoded. 
+  for (var i in data[Object.keys(data)[0]]) {
+    var row = 
+              `<tr>
+              <td>${data['Software Development topics'][i][Object.keys(data[Object.keys(data)[0]][i])[0]]}</td>
+              <td>${data['Software Development topics'][i][Object.keys(data[Object.keys(data)[0]][i])[1]]}</td>              
+              </tr>`;
+    table.innerHTML += row;
+  }
+
+}
 
 
